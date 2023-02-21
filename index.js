d976e661ac638e70e6b10798de3421305e9d0f0a
bituan <113036994+bituann@users.noreply.github.com>
2023-02-21 11:37:24 +0100

    Add node method of receiving input

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// Since this is a node project for now, we'll require input using node methods
+const readline = require("readline");
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
 const choices = ["rock", "paper", "scissors"];
 const winningMatch = {
 	rock: "scissors",
