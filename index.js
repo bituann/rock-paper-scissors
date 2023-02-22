f747ce1a2943845121da5a3200cae0406dbeaaa1
bituan <113036994+bituann@users.noreply.github.com>
2023-02-22 22:02:37 +0100

    Parameterize getComputerChoice, updateScore, and oneRound functions

--- index.js
+++ index.js
@@ -1,90 +1,90 @@
 // Since this is a node project for now, we'll require input using node methods
 const prompt = require("prompt-sync")();
 
-
-
-
 function game() {
 	const choices = ["rock", "paper", "scissors"];
 	const winningMatch = {
-		rock: "scissors", 
-		paper: "rock", 
+		rock: "scissors",
+		paper: "rock",
 		scissors: "paper",
-     }
+	};
 	let scores = {
 		playerScore: 0,
 		computerScore: 0,
-		tie: 0
-	}
+		tie: 0,
+	};
 
 	for (let i = 0; i < 5; i++) {
-		console.log(`Round ${i + 1} of 5`)
-		let playerChoice = prompt("Rock, paper, or scissors? ")
+		console.log(`Round ${i + 1} of 5`);
+		let playerChoice = prompt("Rock, paper, or scissors? ");
 		if (!choices.includes(playerChoice)) {
-			console.log("That's not a valid choice \n")
+			console.log("That's not a valid choice \n");
 			game();
 		}
-		let match = oneRound(playerChoice, getComputerChoice());
+		let match = oneRound(
+			playerChoice,
+			getComputerChoice(choices),
+			winningMatch
+		);
 		console.log(match.message);
-		updateScore(match.outcome);
+		updateScore(match.outcome, scores);
 	}
 
-	let gameOutcome = determineWinner(scores)
-	let outcomeText = announceOutcome(gameOutcome)
-	console.log(outcomeText)
+	let gameOutcome = determineWinner(scores);
+	let outcomeText = announceOutcome(gameOutcome);
+	console.log(outcomeText);
 }
 
-
-
-
-function getComputerChoice() {
+function getComputerChoice(options) {
 	let randomIndex = Math.floor(Math.random() * 3);
-	return choices[randomIndex];
+	return options[randomIndex];
 }
 
-function oneRound(playerChoice, computerChoice) {
+function oneRound(playerChoice, computerChoice, winCombination) {
 	if (computerChoice === playerChoice) {
-		return {outcome: "draw", message: "It's a tie"};
-	} else if (computerChoice === winningMatch[playerChoice]) {
+		return { outcome: "draw", message: "It's a tie" };
+	} else if (computerChoice === winCombination[playerChoice]) {
 		return {
 			outcome: "win",
-			message: `You win! ${playerChoice} beats ${computerChoice}`;
-		}
+			message: `You win! ${playerChoice} beats ${computerChoice}`,
+		};
 	} else {
 		return {
-			outcome: "loss", 
-			message: `You lose! ${computerChoice} beats ${playerChoice}`;
-		}
+			outcome: "loss",
+			message: `You lose! ${computerChoice} beats ${playerChoice}`,
+		};
 	}
 }
 
-function updateScore(oneRoundOutcome) {
-	switch(oneRoundOutcome) {
+function updateScore(oneRoundOutcome, scores) {
+	switch (oneRoundOutcome) {
 		case "draw":
-			scores.tie++
-			break
+			scores.tie++;
+			break;
 		case "win":
-			scores.playerScore++
-			break
+			scores.playerScore++;
+			break;
 		case "loss":
-			scores.computerScore++
-			break
+			scores.computerScore++;
+			break;
 	}
 }
 
 function determineWinner(score) {
-	if (score.playerScore === score.computerScore) return "tie"
-	if (score.playerScore > score.computerScore) return "player"
-	if (score.playerScore < score.computerScore) return "computer"
+	if (score.playerScore === score.computerScore) return "tie";
+	if (score.playerScore > score.computerScore) return "player";
+	if (score.playerScore < score.computerScore) return "computer";
 }
 
 function announceOutcome(outcome) {
 	switch (outcome) {
 		case "tie":
-			return "Evenly Matched! It's a tie."
+			return "Evenly Matched! It's a tie.";
 		case "player":
-			return "Congratulations! You won!"
+			return "Congratulations! You won!";
 		case "computer":
-			return "Too bad, you lost"
+			return "Too bad, you lost";
 	}
 }
+
+game();
